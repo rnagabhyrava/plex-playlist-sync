fd1f5e2d873c4d0aee75a55cb07ccf10bf4dbc24
Makario1337 <74052988+Makario1337@users.noreply.github.com>
2022-11-23 19:13:44 +0100

    Added Regex import...

--- plex-playlist-sync/utils/spotify.py
+++ plex-playlist-sync/utils/spotify.py
@@ -7,6 +7,8 @@ from plexapi.server import PlexServer
 from .helperClasses import Playlist, Track, UserInputs
 from .plex import update_or_create_plex_playlist
 
+import re
+
 
 def _get_sp_user_playlists(
     sp: spotipy.Spotify, user_id: str, suffix: str = " - Spotify"
